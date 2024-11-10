cba80fdbb6dff222ea70d64984253f0975228f34
Pravin Sidwadkar <psidwadkar@gmail.com>
2024-11-10 19:22:46 +0530

    isAbove18() function added and value change

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const age = 25
-const name = 'Pravin Sidwadkar'
+const name1 = 'Pravin Sidwadkar'
 const dob = '01/01/2000'
 
 function sayName ()
 {
-console.log('My Name is' + name)
+console.log('My Name is' + name1)
 
+}
+
+function isAbove18()
+{
+
+    return age >= 18
 }
